363286976d9b5052bd5ef99dfb2698079c7b8cb7
luuke <lukasz.bartosz1990@gmail.com>
2020-04-17 13:50:17 +0200

    Remove variables and prints

--- Wave/Main.cpp
+++ Wave/Main.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
-#include "ConsoleLogger.h"
 
 int main(void)
 {
     std::cout << "Hello World!" << std::endl;
-
-    ILogger *logger = new ConsoleLogger();
-    logger->Log("asd");
-
-    ConsoleLogger consoleLogger;
-    logger->Log("console");
 }
